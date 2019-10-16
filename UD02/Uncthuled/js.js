f613cb220f96cac5bcd448edd41a3718e8394b67
miguelcantos <miguelcantos69@gmail.com>
2019-10-16 16:53:57 +0200

    Creacion e inicio de movimiento

--- UD02/Uncthuled/js.js
+++ UD02/Uncthuled/js.js
@@ -1,5 +1,13 @@
+
+
+window.onload=function(){
+ 
+    listo()
+};
+
 function listo(){
-mapa =[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+mapa =[[3,3,3,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+       [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
        [0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
        [0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -14,21 +22,67 @@ mapa =[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]];
  
  
-for (var i=0;i<13;i++){
+for (var i=0;i<14;i++){
     for (var j=0;j<21;j++){
         var newDiv = document.createElement("div");
-        if (mapa[i][j]==0){
+
+        if(mapa[i][j]==3){
+            newDiv.classList.add("piedra");
+
+        }else if(mapa[i][j]==2){
+            newDiv.classList.add("wazowski");
+
+        }else if (mapa[i][j]==0){
             newDiv.classList.add("camino");
+
         }else{
             newDiv.classList.add("bloque");
+
         }
+
         document.querySelector(".mapa").appendChild(newDiv);
     }
 }
  
 }
- 
-window.onload=function(){
- 
-    listo()
-};
+
+
+function moverte(tecla){
+
+    var key = tecla.keyCode;
+
+    switch (tecla) {
+        case 38:
+            moverteArriba();
+            break;
+
+        case 40:
+            moverteAbajo();
+            break;
+
+        case 39:
+            moverteDerecha();
+            break;
+
+        case 37:
+            moverteIzquierda();
+            break;
+    }
+
+}
+
+moverteAbajo(){
+
+}
+
+moverteArriba(){
+
+}
+
+moverteDerecha(){
+
+}
+
+moverteIzquierda(){
+
+}
