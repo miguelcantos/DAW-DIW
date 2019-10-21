6ccd22e498c4d083371e05968c01f4264931647f
miguelcantos <47533999+miguelcantos@users.noreply.github.com>
2019-10-21 11:59:50 +0200

    Meter sullivan y creacion de mapa con divs
    Proximo, hacer que se mueva wazowski

--- UD02/Uncthuled/js.js
+++ UD02/Uncthuled/js.js
@@ -1,11 +1,4 @@
 
-
-window.onload=function(){
- 
-    listo()
-};
-
-function listo(){
 mapa =[[3,3,3,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
        [0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
@@ -19,31 +12,50 @@ mapa =[[3,3,3,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
        [0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
        [0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-       [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]];
- 
- 
+       [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0]];
+
+window.onload=function(){
+    listo();
+
+}
+
+function listo(){
+  crearMapa();
+
+}
+
+function crearMapa(){
+document.querySelector(".mapa").innerHTML = "";
 for (var i=0;i<14;i++){
     for (var j=0;j<21;j++){
         var newDiv = document.createElement("div");
 
         if(mapa[i][j]==3){
             newDiv.classList.add("piedra");
+            mapa[i][j] = newDiv;
 
         }else if(mapa[i][j]==2){
             newDiv.classList.add("wazowski");
+            mapa[i][j] = newDiv;
 
         }else if (mapa[i][j]==0){
             newDiv.classList.add("camino");
+            mapa[i][j] = newDiv;
+
+        }else if(mapa[i][j]==4){
+            newDiv.classList.add("sullivan");
+            mapa[i][j] = newDiv;
 
         }else{
             newDiv.classList.add("bloque");
+            mapa[i][j] = newDiv;
 
         }
 
         document.querySelector(".mapa").appendChild(newDiv);
     }
 }
- 
+
 }
 
 
@@ -70,7 +82,7 @@ function moverte(tecla){
     }
 
 }
-
+/*
 moverteAbajo(){
 
 }
@@ -86,3 +98,4 @@ moverteDerecha(){
 moverteIzquierda(){
 
 }
+*/
