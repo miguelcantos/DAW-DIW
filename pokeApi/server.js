a8aa3a3be5c47d6df866d1dd1827c34070646f76
miguelcantos <miguelcantos69@gmail.com>
2020-02-19 18:57:36 +0100

    subido

--- pokeApi/server.js
+++ pokeApi/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+let port = process.env.PORT
 
 // Paginas publicas (estaticas)
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Escuchemos en un puerto
-app.listen(3000,() => {
+app.listen(port,() => {
     console.log(" * Miniserver UP and Running en http://localhost:3000");
 });
